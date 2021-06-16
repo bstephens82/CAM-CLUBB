abc62a60fb912d0204f044ebc830da137a5f3fce
Adam Herrington <adamrhster@gmail.com>
2021-06-16 16:56:52 -0600

    moist plumes as default

--- src/physics/cam/clubb_mf.F90
+++ src/physics/cam/clubb_mf.F90
@@ -252,7 +252,7 @@ module clubb_mf
                                              pblhmin  = 100._r8
      !
      ! to condensate or not to condensate
-     logical                              :: do_condensation = .false.
+     logical                              :: do_condensation = .true.
      !
      ! to precip or not to precip
      logical                              :: do_precip = .false.
