cb43f04914a1341c32452493d919b71758da71f8
bstephens82 <benjamin.andrew.stephens@gmail.com>
2023-11-15 13:22:23 -0700

    saving clean_externals.bash

---
+++ clean_externals.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+
+# intended to delete all externals so you can start with a
+# clean slate after checking out a new tag
+
+rm -rf ccs_config
+rm -rf chem_proc
+rm -rf cime
+rm -rf components
+rm -rf libraries
+rm -rf share
+rm -rf post
+rm -rf src/atmos_phys
+rm -rf src/hemco
+rm -rf src/physics/ali_arms
+rm -rf clubb
+rm -rf pumas
+rm -rf pumas-frozen
+rm -rf silhs
+
